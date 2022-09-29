e1d1ce65941e71a98161fea5bf61c9a4347fbf32
ancelmoth <ancelmoth@gmail.com>
2022-09-29 12:43:54 -0300

    comitando enter the commit message for your changes. Lines starting

--- poupanca.js
+++ poupanca.js
@@ -1,7 +1,23 @@
  
 var pegaSaldo = document.getElementById('saldo')
-var pegaValor = document.getElementById('valor')
+var valorHTML = document.getElementById('valor')
 var pegaExtrato = document.getElementById('extrato')
+
+function dataCurta(){
+
+    var data = new Date()
+    var d  = data.getDate()
+    var mes = data.getMonth()+1
+    var ano = data.getFullYear()
+
+    if(d.toString == 1){
+        d = "0" + d
+    }
+    if(mes.toString == 1){
+        mes = "0" + mes
+    }
+    return d + "/" + mes + "/" + ano
+}
 
 
 //OBJETO 
@@ -11,27 +27,27 @@ var poupanca = {
     movimentacao: [],
     depositar: function(){
 
-        var valor = Number(pegaValor.value)//converte a string digitada pelo user no input em number
+        var valor = Number(valorHTML.value)//converte a string digitada pelo user no input em number
         this.saldo += valor; //this é o mesmo que eu escrever poupanca.saldo
       
-        pegaSaldo.innerHTML = this.saldo
-        this.movimentacao.push("Deposito de: " + valor)
-        pegaValor.value = "" //zera a caixa input
+        pegaSaldo.innerHTML = this.saldo.toFixed(2)
+        this.movimentacao.push(dataCurta() + " Deposito de - R$: " + valor.toFixed(2))
+        valorHTML.value = "" //zera a caixa input
 
     },
     sacar: function(){
 
-        var valor1 = Number(pegaValor.value)
+        var valor1 = Number(valorHTML.value)
         if(valor1 > this.saldo){
             alert("Saldo insuficiente..")
         }else{
 
-            this.saldo -= valor1;
+            this.saldo -= valor1.toFixed(2);
             //this.movimentacao.push("Saque de: " + valor);
         }
-        pegaSaldo.innerHTML = this.saldo
-        this.movimentacao.push("Saque de: " + valor1)
-        pegaValor.value = ""
+        pegaSaldo.innerHTML =  this.saldo
+        this.movimentacao.push(dataCurta() + " Saque de R$: " + valor1.toFixed(2))
+        valorHTML.value = ""
 
     },
 
